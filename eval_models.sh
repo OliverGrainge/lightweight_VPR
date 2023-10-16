b04b2216c91a12f6e32a9fdad82be0432af0feb9
oliver grainge <oliver@grainge.me>
2023-10-16 08:28:30 +0100

    Adding Results

--- eval_models.sh
+++ eval_models.sh
@@ -3,6 +3,20 @@
 dataset=pitts30k
 
 #### efficientnet ####
+bb=mobilenetv2conv4
+agg=netvlad
+resume=/home/oliver/Documents/github/lightweight_VPR/weights/fp32/mobilenetv2conv4_netvlad_1024/best_model.pth
+
+python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp32_comp --resume=$resume --fc_output_dim=1024
+python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp16_comp --resume=$resume --fc_output_dim=1024
+python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=int8_comp --resume=$resume --fc_output_dim=1024
+
+git add -u 
+git commit -m "Adding Results"
+git push
+
+
+#### efficientnet ####
 bb=resnet101conv4
 agg=gem
 resume=/home/oliver/Documents/github/lightweight_VPR/weights/fp32/resnet101_netvlad_1024/best_model.pth
@@ -14,6 +28,9 @@ python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --preci
 git add -u 
 git commit -m "Adding Results"
 git push
+
+
+
 
 
 
