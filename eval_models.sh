b857c8f4bdf81e47e4d7df162d2336f1e6eca0f3
oliver grainge <oliver@grainge.me>
2023-10-16 13:57:42 +0100

    Adding Results

--- eval_models.sh
+++ eval_models.sh
@@ -7,6 +7,7 @@ dataset=pitts30k
 bb=resnet101conv4
 agg=gem
 resume=/home/oliver/Documents/github/lightweight_VPR/weights/fp32/resnet101_netvlad_1024/best_model.pth
+#python eval.py --backbone=resnet101conv4 --aggregation=gem --dataset_name=$dataset --precision=fp32_comp --num_workers=16 --save_dir=resnet101_gem_1024 --QAT=False --train_batch_size=2 --infer_batch_size=1 --mixed_precision_training=True --accrue_gradient=1 --fc_output_dim=1024
 python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp32_comp --resume=$resume --fc_output_dim=1024 --mixed_precision_training=True --infer_batch_size=1 --num_workers=16 --QAT=False
 python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp16_comp --resume=$resume --fc_output_dim=1024 --mixed_precision_training=True --infer_batch_size=1 --num_workers=16 --QAT=False
 python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=int8_comp --resume=$resume --fc_output_dim=1024 --mixed_precision_training=True --infer_batch_size=1 --num_workers=16 --QAT=False
@@ -16,20 +17,6 @@ git commit -m "Adding Results"
 git push
 
 
-
-
-#### efficientnet ####
-bb=mobilenetv2conv4
-agg=netvlad
-resume=/home/oliver/Documents/github/lightweight_VPR/weights/fp32/mobilenetv2conv4_netvlad_1024/best_model.pth
-
-python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp32_comp --resume=$resume --fc_output_dim=1024
-python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=fp16_comp --resume=$resume --fc_output_dim=1024
-python eval.py --backbone=$bb --aggregation=$agg --dataset_name=$dataset --precision=int8_comp --resume=$resume --fc_output_dim=1024
-
-git add -u 
-git commit -m "Adding Results"
-git push
 
 
 
--- util.py
+++ util.py
@@ -34,6 +34,7 @@ def save_checkpoint(args, state, is_best, filename):
 
 def resume_model(args, model):
     checkpoint = torch.load(args.resume, map_location=args.device)
+    print(checkpoint.keys())
     if 'model_state_dict' in checkpoint:
         state_dict = checkpoint['model_state_dict']
     else:
